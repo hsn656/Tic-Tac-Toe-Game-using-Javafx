bc6aa42b570aff36044ee2376ecabb781f5c4799
hsn656 <hassan.mhmd656@gmail.com>
2022-02-10 23:24:09 +0200

    sql statements to create mysql db

---
+++ data base creation.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `user_base` ;
+
+CREATE TABLE IF NOT EXISTS `player` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  `points` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
